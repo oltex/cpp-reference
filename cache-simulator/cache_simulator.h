af76dead013e1dd539cdf47b0ca927a4ef92517c
oltex <80347929+oltex@users.noreply.github.com>
2024-07-29 10:25:01 +0900

    240729

--- cache-simulator/cache_simulator.h
+++ cache-simulator/cache_simulator.h
@@ -6,68 +6,43 @@
 
 #include <iostream>
 
-//class tree_plur final {
-//public:
-//	inline void select(size_t const idx) noexcept {
-//		auto parent = (idx - (idx % 2 ? 1 : 0) + _size) / 2;
-//		auto child = idx;
-//		while (parent > 0) {
-//			size_t bit = 1 << (_size - parent - 1);
-//			if (child % 2)
-//				_bit &= ~bit;
-//			else
-//				_bit |= bit;
-//
-//			child = parent;
-//			parent = (child) / 2;
-//		}
-//	}
-//	inline auto replace(void) noexcept -> size_t {
-//		size_t parent = 1;
-//
-//		while (parent < _size) {
-//			size_t bit = 1 << (_size - parent - 1);
-//			if (_bit & bit)
-//				_bit &= ~bit;
-//			else
-//				_bit |= bit;
-//			parent = parent * 2 + (_bit & bit ? 0 : 1);
-//		}
-//
-//		return parent - _size;
-//	}
-//private:
-//	unsigned short _bit;
-//};
+
 
 class cache_simulator final : public singleton<cache_simulator> {
 	friend class singleton<cache_simulator>;
 private:
-	struct cache_line {
-		size_t _tag;
-	};
-	class cache_way final {
+	using cache_line = size_t;
+	class cache_way {
 	public:
 		inline explicit cache_way(size_t const size, unsigned long const tag) noexcept
 			: _size(size), _shift(tag) {
 			_line = (cache_line*)malloc(sizeof(cache_line) * size);
 			memset(_line, 1, sizeof(cache_line) * size);
 		}
-		inline ~cache_way(void) noexcept {
+		inline virtual ~cache_way(void) noexcept {
 			free(_line);
 		}
 	public:
-		inline void access(void const* const ptr) noexcept {
+		inline virtual void access(void const* const ptr) noexcept = 0;
+	protected:
+		cache_line* _line;
+		size_t const _size;
+		unsigned long const _shift;
+	};
+	class tree_plur final : public cache_way {
+	public:
+		using cache_way::cache_way;
+	public:
+		inline virtual void access(void const* const ptr) noexcept override {
 			auto tag = (size_t)ptr >> _shift;
 			for (size_t i = 0; i < _size; ++i)
-				if (_line[i]._tag == tag) {
+				if (_line[i] == tag) {
 					select(i);
 					std::cout << "캐시히트" << std::endl;
 					return;
 				}
 			auto idx = replace();
-			_line[idx]._tag = tag;
-
+			_line[idx] = tag;
 			std::cout << "캐시미스" << std::endl;
 		}
 	private:
@@ -75,11 +50,11 @@ private:
 			auto parent = (idx - (idx % 2 ? 1 : 0) + _size) / 2;
 			auto child = idx;
 			while (parent > 0) {
-				size_t bit = 1 << (_size - parent - 1);
+				unsigned short bit = 1 << (_size - parent - 1);
 				if (child % 2)
-					_plru &= ~bit;
+					_bit &= ~bit;
 				else
-					_plru |= bit;
+					_bit |= bit;
 
 				child = parent;
 				parent = (child) / 2;
@@ -89,31 +64,24 @@ private:
 			size_t parent = 1;
 
 			while (parent < _size) {
-				size_t bit = 1 << (_size - parent - 1);
-				if (_plru & bit)
-					_plru &= ~bit;
+				unsigned short bit = 1 << (_size - parent - 1);
+				if (_bit & bit)
+					_bit &= ~bit;
 				else
-					_plru |= bit;
-				parent = parent * 2 + (_plru & bit ? 0 : 1);
+					_bit |= bit;
+				parent = parent * 2 + (_bit & bit ? 0 : 1);
 			}
 
 			return parent - _size;
 		}
 	private:
-		cache_line* _line;
-		size_t const _size;
-
-		unsigned long const _shift;
-		unsigned short _plru = 0;
+		unsigned short _bit = 0;
 	};
 	class cache_set final {
+		friend class cache_simulator;
 	public:
-		inline explicit cache_set(size_t const set, size_t mask, unsigned long idx,
-			size_t const way, unsigned long tag) noexcept
-			: _size(set), _mask(mask), _shift(idx) {
-			_way = (cache_way*)malloc(sizeof(cache_way) * set);
-			for (size_t i = 0; i < set; ++i)
-				new(_way + i) cache_way(way, tag);
+		inline explicit cache_set(size_t const set, size_t mask, unsigned long line) noexcept
+			: _size(set), _mask(mask), _shift(line) {
 		}
 		inline ~cache_set(void) noexcept {
 			for (size_t i = 0; i < _size; ++i)
@@ -128,7 +96,6 @@ private:
 	private:
 		cache_way* _way;
 		size_t const _size;
-
 		size_t const _mask;
 		unsigned long const _shift;
 	};
@@ -138,18 +105,18 @@ private:
 		__cpuidex(reg, 0x00000004, 0x00000000);
 
 		size_t set = reg[2] + 1;
+		unsigned long line = (reg[1] & 0xfff) + 1;
+		_BitScanReverse(&line, line);
+		size_t mask = (set - 1) << line;
+		_set = new cache_set(set, mask, line);
+
 		size_t way = ((reg[1] & 0xffc00000) >> 22) + 1;
-		size_t line = (reg[1] & 0xfff) + 1;
-
-		unsigned long idx;
-		_BitScanReverse(&idx, line);
-		size_t mask = (set - 1) << idx;
-
 		unsigned long tag;
 		_BitScanReverse(&tag, set);
-		tag += idx;
-
-		_set = new cache_set(set, mask, idx, way, tag);
+		tag += line;
+		_set->_way = (tree_plur*)malloc(sizeof(tree_plur) * set);
+		for (size_t i = 0; i < set; ++i)
+			new(_set->_way + i) tree_plur(way, tag);
 	}
 	inline ~cache_simulator(void) noexcept {
 		delete _set;
