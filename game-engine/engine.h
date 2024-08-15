10c533c7403667960c4a8ae0e1695b53d05f402f
user <user@DESKTOP-2PAL0L0>
2024-08-15 20:28:01 +0900

    240815

--- game-engine/engine.h
+++ game-engine/engine.h
@@ -3,7 +3,7 @@
 
 #include "object_manager.h"
 #include "component_manager.h"
-#include "frame_manager.h"
+#include "timer_manager.h"
 
 #pragma comment(lib, "Winmm.lib")
 #include <Windows.h>
@@ -15,32 +15,33 @@ namespace engine {
 		inline explicit engine(void) noexcept
 			: _object_manager(object_manager::instance()),
 			_component_manager(component_manager::instance()),
-			_frame_manager(frame_manager::instance()) {
+			_timer_manager(timer_manager::instance()) {
 
 			timeBeginPeriod(1);
 		};
 		inline ~engine(void) noexcept = default;
 	public:
 		inline void update(void) const noexcept {
-			_frame_manager.set_frame(1);
+			_timer_manager.set_frame(1);
 			for (;;) {
 
-				//test
-				//static int _update_tick = 0;
-				//static long _time = timeGetTime();
-				//_update_tick++;
-				//long time = timeGetTime();
-				//if (time - _time >= 1000) {
-				//	_time += 1000;
-				//	_update_tick = 0;
-				//}
+				static int _update_tick = 0;
+				static long _time = timeGetTime();
+				_update_tick++;
+				long time = timeGetTime();
+				if (time - _time >= 1000) {
+					_time += 1000;
+					std::cout << _update_tick << std::endl;
+					_update_tick = 0;
+				}
+				std::cout << _timer_manager.get_delta() << std::endl;
 
-				_frame_manager.update();
+				_timer_manager.update();
 			}
 		};
 	private:
 		object_manager& _object_manager;
 		component_manager& _component_manager;
-		frame_manager& _frame_manager;
+		timer_manager& _timer_manager;
 	};
 }
--- game-engine/timer_manager.h
+++ game-engine/timer_manager.h
@@ -1,40 +1,36 @@
 #pragma once
 #include "design-pattern/singleton.h"
-#include <chrono>
-#include <thread>
+#include "timer.h"
 
 namespace engine {
 	class timer_manager final : public singleton<timer_manager> {
 		friend class singleton<timer_manager>;
 		friend class engine;
 	private:
-		inline explicit timer_manager(void) noexcept
-			: _time(std::chrono::high_resolution_clock::now()) {
+		inline explicit timer_manager(void) noexcept {
 		}
 		inline ~timer_manager(void) noexcept {
 		}
 	public:
 		inline void update(void) noexcept {
 			auto time = std::chrono::high_resolution_clock::now();
-			_delta = std::chrono::duration_cast<std::chrono::nanoseconds>(time - _time);
-			_time += _frame;
-
-			if (_delta < _frame)
-				std::this_thread::sleep_for(std::chrono::nanoseconds(_frame - _delta));
+			_delta.update(time);
+			_frame.update(time);
+			if (_frame.get_delta() < _frame.get_frame())
+				std::this_thread::sleep_for(std::chrono::nanoseconds(_frame.get_frame() - _frame.get_delta()));
 		}
 	public:
 		inline void set_frame(long long frame) noexcept {
-			_frame = std::chrono::nanoseconds(long long(1000000000. / frame));
+			_frame.set_frame(std::chrono::nanoseconds(long long(1000000000. / frame)));
 		}
 		inline auto get_frame(void) const noexcept -> long long {
-			return static_cast<long long>(1000000000. / _frame.count());
+			return static_cast<long long>(1000000000. / _frame.get_frame().count());
 		}
-		inline auto get_delta(void) const noexcept {
-			return _delta;
+		inline auto get_delta(void) const noexcept -> float {
+			return  std::chrono::duration_cast<std::chrono::duration<float>>(_delta.get_delta()).count();
 		}
 	private:
-		std::chrono::high_resolution_clock::time_point _time;
-		std::chrono::nanoseconds _frame;
-		std::chrono::nanoseconds _delta;
+		timer _frame;
+		timer _delta;
 	};
 }
