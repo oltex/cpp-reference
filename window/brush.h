69bd7a6e58b7fc64e831c041ea976cc5bb4833ca
user <user@DESKTOP-2PAL0L0>
2024-08-19 20:01:55 +0900

    240819

--- window/brush.h
+++ window/brush.h
@@ -4,8 +4,16 @@
 namespace window {
 	class brush final : public object {
 	public:
-		inline explicit brush(COLORREF const color) noexcept {
-			_hgdiobj = CreateSolidBrush(color);
+		inline explicit brush(COLORREF const color) noexcept
+			: object(CreateSolidBrush(color)) {
+		};
+		inline explicit brush(brush const& rhs) noexcept = delete;
+		inline auto operator=(brush const& rhs) noexcept -> brush & = delete;
+		inline explicit brush(brush&& rhs) noexcept
+			: object(std::move(rhs)) {
+		};
+		inline auto operator=(brush&& rhs) noexcept -> brush& {
+			object::operator=(std::move(rhs));
 		};
 		inline virtual ~brush(void) noexcept override = default;
 	};
--- window/object.h
+++ window/object.h
@@ -4,9 +4,19 @@
 namespace window {
 	class object {
 	public:
-		inline explicit object(void) noexcept = default;
+		inline explicit object(HGDIOBJ hgdiobj) noexcept 
+			: _hgdiobj(hgdiobj) {
+		};
 		inline explicit object(object const& rhs) noexcept = delete;
 		inline auto operator=(object const& rhs) noexcept -> object & = delete;
+		inline explicit object(object&& rhs) noexcept
+			: _hgdiobj(rhs._hgdiobj) {
+			rhs._hgdiobj = nullptr;
+		};
+		inline auto operator=(object&& rhs) noexcept -> object& {
+			_hgdiobj = rhs._hgdiobj;
+			rhs._hgdiobj = nullptr;
+		};
 		inline virtual ~object(void) noexcept {
 			DeleteObject(_hgdiobj);
 		}
@@ -15,7 +25,7 @@ namespace window {
 			return _hgdiobj;
 		}
 	protected:
-		HGDIOBJ _hgdiobj = nullptr;
+		HGDIOBJ _hgdiobj;
 	};
 }
 
