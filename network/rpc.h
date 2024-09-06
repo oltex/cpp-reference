009413a1be08a2a1fe701fc2c2017b47a5a1ed54
user <user@DESKTOP-2PAL0L0>
2024-09-06 21:11:06 +0900

    240906

---
+++ network/rpc.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace network {
+	class remote_procedure_call final {
+		enum type {
+		};
+	public:
+		inline explicit remote_procedure_call(void) noexcept = default;
+		inline ~remote_procedure_call(void) noexcept = default;
+	public:
+
+	};
+}
