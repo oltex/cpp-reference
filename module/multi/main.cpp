190ab675111ecab719b94dad111a4405ae7c1c3e
oltex <80347929+oltex@users.noreply.github.com>
2024-11-09 21:22:40 +0900

    241109

--- module/multi/main.cpp
+++ module/multi/main.cpp
@@ -4,10 +4,10 @@
 
 #include "thread.h"
 
-#include "spin_lock.h"
+#include "spin.h"
 #include "wait_on_address_lock.h"
 #include "critical_section.h"
-#include "slim_read_write_lock.h"
+#include "slim_read_write.h"
 
 #include "ordering.h"
 
@@ -17,30 +17,20 @@
 #include <iostream>
 #include <conio.h>
 
-std::once_flag flag;
 
-void once(void) noexcept {
-	std::cout << "a" << std::endl;
-}
-
-void function(void) noexcept {
+void function(int& a) noexcept {
 	__debugbreak();
-	std::call_once(flag, once);
 	std::cout << "b" << std::endl;
 }
 
 int main(void) noexcept {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-
-	multi::thread thread1(function, 0);
-	multi::thread thread2(function, 0);
-
+	int a = 10;
+	std::thread thread1(function, a);
+	std::thread thread2(function, a);
 	system("pause");
-	thread1.resume();
-	thread2.resume();
-
-	thread1.join(INFINITE);
-	thread2.join(INFINITE);
+	thread1.join();
+	thread2.join();
 	system("pause");
 	return 0;
 }
