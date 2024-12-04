025b0cf9b04277f77e558db645af1eaad4cc8660
user <user@DESKTOP-2PAL0L0>
2024-12-04 21:14:15 +0900

    241204

--- algorithm/lockfree-stack/lockfree_stack.h
+++ algorithm/lockfree-stack/lockfree_stack.h
@@ -1,8 +1,11 @@
 #pragma once
+#include "../../module/multi/spin.h"
+#include "../../module/multi/wait_on_address_lock.h"
 #include "logger.h"
 #include <utility>
 #include <Windows.h>
 #include <intrin.h>
+#include <string>
 
 class lockfree_stack final {
 private:
@@ -10,6 +13,14 @@ private:
 	struct node final {
 		node* _next;
 		int _value;
+	};
+	struct log final {
+		unsigned long _thread_id;
+		wchar_t const* _action;
+
+		void* _head = 0;
+		void* _current = 0;
+		void* _next = 0;
 	};
 public:
 	inline explicit lockfree_stack(void) noexcept
@@ -25,41 +36,47 @@ public:
 		node* current = reinterpret_cast<node*>(malloc(sizeof(node)));
 		current->_value = value;
 		for (;;) {
-			_logger.log(L"[thread : %05d] push doing : %08d | start   | currrent->next = _head", GetCurrentThreadId(), value);
 			current->_next = _head;
-			_logger.log(L"[thread : %05d] push doing : %08d | end     | currrent->next = _head", GetCurrentThreadId(), value);
-			_logger.log(L"[thread : %05d] push doing : %08d | start   | _head == current->next ? current", GetCurrentThreadId(), value);
-			auto head = _InterlockedCompareExchangePointer(reinterpret_cast<void* volatile*>(&_head), current, current->_next);
-			_logger.log(L"[thread : %05d] push doing : %08d | end     | _head == current->next ? current", GetCurrentThreadId(), value);
+
+			node* head = (node*)_InterlockedCompareExchangePointer(reinterpret_cast<void* volatile*>(&_head), current, current->_next);
 			if (current->_next == head) {
-				_logger.log(L"[thread : %05d] push doing : %08d | success", GetCurrentThreadId(), value);
+				//{
+				//	auto order = _InterlockedIncrement(&_order);
+				//	_log[order]._thread_id = GetCurrentThreadId();
+				//	_log[order]._action = L"push : if (current->_next == head) turn";
+				//	_log[order]._head = (void*)head;
+				//	_log[order]._current = (void*)current;
+				//}
 				break;
 			}
-			else
-				_logger.log(L"[thread : %d] push doing : %08d | fail", GetCurrentThreadId(), value);
 		}
 	}
 	inline auto pop(void) noexcept -> int {
-		int value;
+		int result;
 		for (;;) {
-			_logger.log(L"[thread : %05d] pop  doing : 00000000 | start   | current = _head", GetCurrentThreadId());
 			auto current = _head;
-			value = current->_value;
-			_logger.log(L"[thread : %05d] pop  doing : %08d | start   | current = _head", GetCurrentThreadId(), value);
-			_logger.log(L"[thread : %05d] pop  doing : %08d | start   | _head == current ? current->next", GetCurrentThreadId(), value);
-			auto head = _InterlockedCompareExchangePointer(reinterpret_cast<void* volatile*>(&_head), current->_next, current);
-			_logger.log(L"[thread : %05d] pop  doing : %08d | end     | _head == current ? current->next", GetCurrentThreadId(), value);
+			auto next = current->_next;
+
+			node* head = (node*)_InterlockedCompareExchangePointer(reinterpret_cast<void* volatile*>(&_head), next, current);
 			if (current == head) {
-				_logger.log(L"[thread : %05d] pop  doing : %08d | success", GetCurrentThreadId(), value);
-				delete current;
-				return value;
-			}
-			else {
-				_logger.log(L"[thread : %05d] pop  doing : %08d | fail", GetCurrentThreadId(), value);
+				{
+					auto order = _InterlockedIncrement(&_order);
+					_log[order]._thread_id = GetCurrentThreadId();
+					_log[order]._action = L"pop : if (current == head) true";
+					_log[order]._head = (void*)head;
+					_log[order]._next = (void*)next;
+					_log[order]._current = (void*)current;
+					//if (head->_next != next)
+					//	__debugbreak();
+				}
+				result = current->_value;
+				free(current);
+				return result;
 			}
 		}
 	}
 private:
 	node* _head;
-	logger& _logger = logger::instance();
+	volatile unsigned int _order = 0;
+	log* _log = new log[30000000];
 };
--- algorithm/lockfree-stack/main.cpp
+++ algorithm/lockfree-stack/main.cpp
@@ -1,35 +1,30 @@
 #include "lockfree_stack.h"
-#include "lockfree_stack2.h"
 #include "logger.h"
 #include <thread>
 #include <intrin.h>
 #include <Windows.h>
 
 logger& _logger = logger::instance();
-lockfree_stack2 _lockfree_stack;
+lockfree_stack _lockfree_stack;
 volatile unsigned int _value = 0;
 
 inline static unsigned int __stdcall func(void* arg) noexcept {
 	for (;;) {
+		//for (int i = 0; i < 10; ++i) {
+		//	auto value = _InterlockedIncrement(&_value);
+		//	_lockfree_stack.push(value);
+		//}
 		for (int i = 0; i < 10; ++i) {
-			auto value = _InterlockedIncrement(&_value);
-			//_logger.log(L"[thread : %05d] push start : %08d", GetCurrentThreadId(), value);
-			_lockfree_stack.push(value);
-			//_logger.log(L"[thread : %05d] push end   : %08d", GetCurrentThreadId(), value);
-		}
-		for (int i = 0; i < 10; ++i) {
-			//_logger.log(L"[thread : %05d] pop  start", GetCurrentThreadId());
 			_lockfree_stack.pop();
-			//_logger.log(L"[thread : %05d] pop  end", GetCurrentThreadId());
 		}
 	}
 }
 
 int main(void) noexcept {
-	//for (int i = 0; i < 100000000; ++i) {
-	//	auto value = _InterlockedIncrement(&_value);
-	//	_lockfree_stack.push(value);
-	//}
+	for (int i = 0; i < 10000000; ++i) {
+		auto value = _InterlockedIncrement(&_value);
+		_lockfree_stack.push(value);
+	}
 	HANDLE _handle0 = (HANDLE)_beginthreadex(nullptr, 0, func, nullptr, 0, 0);
 	HANDLE _handle1 = (HANDLE)_beginthreadex(nullptr, 0, func, nullptr, 0, 0);
 	Sleep(INFINITE);
