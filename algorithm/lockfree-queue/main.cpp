d4f9242ad53a5659bde29b200d7a9dee19a02d5e
oltex <80347929+oltex@users.noreply.github.com>
2024-12-17 03:00:10 +0900

    241217

--- algorithm/lockfree-queue/main.cpp
+++ algorithm/lockfree-queue/main.cpp
@@ -11,15 +11,15 @@ volatile unsigned int _value = 0;
 inline static unsigned int __stdcall func(void* arg) noexcept {
 	int count = 0;
 	for (;;) {
-		//if (count++ == 10000) {
-		//	std::cout << "thread :" << GetCurrentThreadId() /*<< "size :" << _lockfree_queue._size */<< std::endl;
-		//	count = 0;
-		//}
-		for (int i = 0; i < 3; ++i) {
+		if (count++ == 100000) {
+			std::cout << "thread :" << GetCurrentThreadId() /*<< "size :" << _lockfree_queue._size */<< std::endl;
+			count = 0;
+		}
+		for (int i = 0; i < 2; ++i) {
 			//auto value = _InterlockedIncrement(&_value);
 			_lockfree_queue.push(1);
 		}
-		for (int i = 0; i < 3; ++i) {
+		for (int i = 0; i < 2; ++i) {
 			_lockfree_queue.pop();
 		}
 	}
@@ -52,8 +52,12 @@ inline static unsigned int __stdcall func_pop2(void* arg) noexcept {
 }
 
 int main(void) noexcept {
-	HANDLE _handle0 = reinterpret_cast<HANDLE>(_beginthreadex(nullptr, 0, func, nullptr, 0, 0));
-	HANDLE _handle1 = reinterpret_cast<HANDLE>(_beginthreadex(nullptr, 0, func, nullptr, 0, 0));
+	HANDLE _handle0 = reinterpret_cast<HANDLE>(_beginthreadex(nullptr, 0, func, nullptr, 0/*CREATE_SUSPENDED*/, 0));
+	HANDLE _handle1 = reinterpret_cast<HANDLE>(_beginthreadex(nullptr, 0, func, nullptr, 0/*CREATE_SUSPENDED*/, 0));
+	//system("pause");
+	//ResumeThread(_handle0);
+	//ResumeThread(_handle1);
+
 	//HANDLE _handle2 = reinterpret_cast<HANDLE>(_beginthreadex(nullptr, 0, func, nullptr, 0, 0));
 	//HANDLE _handle3 = reinterpret_cast<HANDLE>(_beginthreadex(nullptr, 0, func, nullptr, 0, 0));
 	//HANDLE _handle4 = reinterpret_cast<HANDLE>(_beginthreadex(nullptr, 0, func, nullptr, 0, 0));
