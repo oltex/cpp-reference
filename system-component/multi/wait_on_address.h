2d3d1f496f15381ce0685bb42c561334beb32ab7
user <user@DESKTOP-2PAL0L0>
2025-01-09 23:26:22 +0900

    250109

--- system-component/multi/wait_on_address.h
+++ system-component/multi/wait_on_address.h
@@ -13,8 +13,7 @@ namespace system_component::multi {
 		inline ~wait_on_address(void) noexcept = default;
 	public:
 		inline void wait(unsigned long milli_second) noexcept {
-			volatile long compare = 1;
-			WaitOnAddress(&_address, (void*)&compare, sizeof(long), milli_second);
+			WaitOnAddress(&_address, (void*)&_compare, sizeof(long), milli_second);
 		}
 		inline void wake_single(void) noexcept {
 			WakeByAddressSingle((void*)&_address);
@@ -22,7 +21,11 @@ namespace system_component::multi {
 		inline void wake_all(void) noexcept {
 			WakeByAddressAll((void*)&_address);
 		}
+		inline void set_compare(long comapre) noexcept {
+			_compare = comapre;
+		}
 	private:
 		volatile long _address;
+		volatile long _compare = 1;
 	};
 }
