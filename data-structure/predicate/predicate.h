2f09613927f16ff1e077f4e0a2600d8b1900bd9e
oltex <80347929+oltex@users.noreply.github.com>
2025-01-30 17:56:23 +0900

    250130

--- data-structure/predicate/predicate.h
+++ data-structure/predicate/predicate.h
@@ -1,16 +1,18 @@
 #pragma once
 #include <compare>
 
-template<typename type>
-struct less final {
-	inline auto operator()(type const& source, type const& destination) const noexcept {
-		return source <=> destination;
-	}
-};
+namespace predicate {
+	template<typename type>
+	struct less final {
+		inline auto operator()(type const& source, type const& destination) const noexcept {
+			return source <=> destination;
+		}
+	};
 
-template<typename type>
-struct greater final {
-	inline auto operator()(type const& source, type const& destination) const noexcept {
-		return destination <=> source;
-	}
-};
+	template<typename type>
+	struct greater final {
+		inline auto operator()(type const& source, type const& destination) const noexcept {
+			return destination <=> source;
+		}
+	};
+}
--- system-component/multi/wait_on_address.h
+++ system-component/multi/wait_on_address.h
@@ -12,20 +12,14 @@ namespace system_component::multi {
 		inline auto operator=(wait_on_address&& rhs) noexcept -> wait_on_address & = delete;
 		inline ~wait_on_address(void) noexcept = default;
 	public:
-		inline void wait(unsigned long milli_second) noexcept {
-			WaitOnAddress(&_address, (void*)&_compare, sizeof(long), milli_second);
+		inline void wait(volatile long& address, volatile long _compare, unsigned long milli_second) noexcept {
+			WaitOnAddress(&address, (void*)&_compare, sizeof(long), milli_second);
 		}
-		inline void wake_single(void) noexcept {
-			WakeByAddressSingle((void*)&_address);
+		inline void wake_single(volatile long& address) noexcept {
+			WakeByAddressSingle((void*)&address);
 		}
-		inline void wake_all(void) noexcept {
-			WakeByAddressAll((void*)&_address);
+		inline void wake_all(volatile long& address) noexcept {
+			WakeByAddressAll((void*)&address);
 		}
-		inline void set_compare(long comapre) noexcept {
-			_compare = comapre;
-		}
-	private:
-		volatile long _address;
-		volatile long _compare = 1;
 	};
 }
