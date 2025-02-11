ee2d43012a15ffe1b0297c38200f1e32d0c2c58b
user <user@DESKTOP-2PAL0L0>
2025-02-11 16:47:16 +0900

    250211

--- system-component/time/multimedia.h
+++ system-component/time/multimedia.h
@@ -2,53 +2,30 @@
 #include <Windows.h>
 #pragma comment(lib, "Winmm.lib")
 
-namespace system_component::time {
-	class multimedia final {
-	public:
-		inline explicit multimedia(void) noexcept = default;
-		inline explicit multimedia(multimedia const& rhs) noexcept
-			: _time(rhs._time) {
-		};
-		inline explicit multimedia(multimedia&& rhs) noexcept
-			: _time(rhs._time) {
-		}
-		inline auto operator=(multimedia const& rhs) noexcept -> multimedia& {
-			_time = rhs._time;
-		}
-		inline auto operator=(multimedia&& rhs) noexcept -> multimedia& {
-			_time = rhs._time;
-		}
-		inline ~multimedia(void) noexcept = default;
-	public:
-		inline auto get_time(void) noexcept -> unsigned long {
-			return _time = timeGetTime();
-		}
-		inline auto data(void) noexcept -> unsigned long& {
-			return _time;
-		}
-	private:
-		unsigned long _time;
-	public:
-		inline static void begin_period(unsigned int peroid) noexcept {
-			timeBeginPeriod(peroid);
-		}
-		inline static void end_period(unsigned int peroid) noexcept {
-			timeEndPeriod(peroid);
-		}
-	};
+namespace system_component::time::multimedia {
+	inline static auto get_time(void) noexcept -> unsigned long {
+		return timeGetTime();
+	}
+	inline static void begin_period(unsigned int peroid) noexcept {
+		timeBeginPeriod(peroid);
+	}
+	inline static void end_period(unsigned int peroid) noexcept {
+		timeEndPeriod(peroid);
+	}
+};
 
-	//namespace period {
-	//	//class raii final {
-	//	//public:
-	//	//	inline explicit raii(unsigned int peroid) noexcept
-	//	//		: _peroid(peroid) {
-	//	//		timeBeginPeriod(peroid);
-	//	//	}
-	//	//	inline ~raii(void) noexcept {
-	//	//		timeEndPeriod(_peroid);
-	//	//	}
-	//	//private:
-	//	//	unsigned int const _peroid;
-	//	//};
-	//}
+//namespace period {
+//	//class raii final {
+//	//public:
+//	//	inline explicit raii(unsigned int peroid) noexcept
+//	//		: _peroid(peroid) {
+//	//		timeBeginPeriod(peroid);
+//	//	}
+//	//	inline ~raii(void) noexcept {
+//	//		timeEndPeriod(_peroid);
+//	//	}
+//	//private:
+//	//	unsigned int const _peroid;
+//	//};
+//}
 }
