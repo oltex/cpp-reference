af57f9ef8c5096c59a54639e2c986b32e7eba59d
user <user@DESKTOP-2PAL0L0>
2025-02-17 20:26:24 +0900

    250117

--- database/redis/main.cpp
+++ database/redis/main.cpp
@@ -4,9 +4,22 @@
 #pragma comment (lib, "tacopie.lib")
 #pragma comment (lib, "ws2_32.lib")
 
+int main()
+{
+	WORD version = MAKEWORD(2, 2);
+	WSADATA data;
+	WSAStartup(version, &data);
 
-#include <iostream>
+	cpp_redis::client client;
+	client.connect();
 
-int main(void)noexcept {
-	return 0;
+	client.set("hello", "42");
+	client.get("hello", [](cpp_redis::reply& reply) {
+		std::cout << reply << std::endl;
+		});
+	//! also support std::future
+	//! std::future<cpp_redis::reply> get_reply = client.get("hello");
+
+	client.sync_commit();
+	//! or client.commit(); for asynchronous call}
 }
