16f3dad4d51b339111e61b758f1bdc3c68fdfb2f
user <user@DESKTOP-2PAL0L0>
2025-02-17 20:21:52 +0900

    250217

---
+++ database/redis/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cpp_redis/cpp_redis>
+#pragma comment (lib, "cpp_redis.lib")
+#pragma comment (lib, "tacopie.lib")
+#pragma comment (lib, "ws2_32.lib")
+
+
+#include <iostream>
+
+int main(void)noexcept {
+	return 0;
+}
