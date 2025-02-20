16ee510f4cb9aa8476b7836cb24fe0634293b285
user <user@DESKTOP-2PAL0L0>
2025-02-20 21:37:56 +0900

    250220

--- system-component/coroutine/main.cpp
+++ system-component/coroutine/main.cpp
@@ -24,20 +24,41 @@ class Derived : public Base {
 	//}
 };
 
+//int main() {
+//	Base* obj = new Base();
+//	system("pause");
+//
+//	for (int j = 0; j < 10; j++) {
+//		auto rdtsc = __rdtsc();
+//		for (int i = 0; i < 1000000; ++i) {
+//			//obj->coroutineFunction2();
+//			auto task = obj->coroutineFunction();
+//			task.resume();
+//		}
+//		rdtsc = __rdtsc() - rdtsc;
+//		std::cout << rdtsc << std::endl;
+//	}
+//
+//	delete obj;
+//}
+
+system_component::coroutine<system_component::promise> test(void) {
+	//1
+	co_await; //string //디비 요청
+	//3
+	co_return;
+}
+
 int main() {
-	Base* obj = new Base();
-	system("pause");
+	// gqcs
 
-	for (int j = 0; j < 10; j++) {
-		auto rdtsc = __rdtsc();
-		for (int i = 0; i < 1000000; ++i) {
-			//obj->coroutineFunction2();
-			auto task = obj->coroutineFunction();
-			task.resume();
-		}
-		rdtsc = __rdtsc() - rdtsc;
-		std::cout << rdtsc << std::endl;
-	}
+	auto co = test();
+	co.resume();
+	//2 디비조회를 요청했구나!
+	// 디비조회를 해줘 redis [](){끝나면 pqcs *co 쏴줘};
+	continue;
 
-	delete obj;
+
+	// 디비작업이 완료됐구나!
+	co.resume();
 }
