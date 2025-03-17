b425b26652a8f254126a61c818c398f96f3560f6
user <user@DESKTOP-2PAL0L0>
2025-03-17 19:40:39 +0900

    250317

--- algorithm/least-common-multiple/least_common_multiple.h
+++ algorithm/least-common-multiple/least_common_multiple.h
@@ -1,1 +1,15 @@
 #pragma once
+#include <concepts>
+#include "../greatest-common-divisor/greatest_common_divisor.h"
+
+namespace algorithm {
+	template <typename type>
+	inline constexpr auto least_common_multiple(type first, type second) noexcept {
+		return first* second / greatest_common_divisor(first, second);
+	}
+
+	template <typename type_1, typename... type_2>
+	inline constexpr auto least_common_multiple(type_1 first, type_2... second) noexcept {
+		return least_common_multiple(first, least_common_multiple(second...));
+	}
+}
---
+++ algorithm/least-common-multiple/main.cpp
@@ -0,0 +1,5 @@
+#include "least_common_multiple.h"
+
+int main(void) noexcept {
+	int result = algorithm::least_common_multiple(4, 2, 3, 5, 6, 7);
+}
