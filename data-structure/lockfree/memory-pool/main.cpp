5ecedd7361d91449ee5601062c44ed535e9004c9
user <user@DESKTOP-2PAL0L0>
2025-04-11 20:31:53 +0900

    250411

--- data-structure/lockfree/memory-pool/main.cpp
+++ data-structure/lockfree/memory-pool/main.cpp
@@ -38,15 +38,15 @@ inline static unsigned int __stdcall func(void* arg) noexcept {
 int main(void) noexcept {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	std::vector<int*> _vector;
-	for (int i = 0; i < 10000; ++i) {
-		_vector.emplace_back(&_memory_pool.allocate());
-	}
-	for (auto i = 0; i < 10000; ++i) {
-		int* a = _vector.back();
-		_memory_pool.deallocate(*a);
-		_vector.pop_back();
-	}
+	//std::vector<int*> _vector;
+	//for (int i = 0; i < 10000; ++i) {
+	//	_vector.emplace_back(&_memory_pool.allocate());
+	//}
+	//for (auto i = 0; i < 10000; ++i) {
+	//	int* a = _vector.back();
+	//	_memory_pool.deallocate(*a);
+	//	_vector.pop_back();
+	//}
 
 	HANDLE _handle0 = (HANDLE)_beginthreadex(nullptr, 0, func, nullptr, 0, 0);
 	HANDLE _handle1 = (HANDLE)_beginthreadex(nullptr, 0, func, nullptr, 0, 0);
