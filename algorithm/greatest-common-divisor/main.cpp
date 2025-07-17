cfb32561d3ff5afa10306606d3e4ec63d6e594df
oltex <80347929+oltex@users.noreply.github.com>
2025-07-17 19:44:25 +0900

    250717

--- algorithm/greatest-common-divisor/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "greatest_common_divisor.h"
-#include <numeric>
-int main(void) noexcept {
-	int a = library::greatest_common_divisor(24, 36, 48, 60);
-	std::gcd(1, 2);
-
-	return 0;
-}
--- function/function.h
+++ function/function.h
@@ -74,6 +74,19 @@ namespace library {
 	inline constexpr auto least_common_multiple(type_1 first, type_2... second) noexcept {
 		return least_common_multiple(first, least_common_multiple(second...));
 	}
+	template <typename type>
+	inline constexpr auto greatest_common_divisor(type first, type second) noexcept {
+		while (second != 0) {
+			type temp = second;
+			second = first % second;
+			first = temp;
+		}
+		return first;
+	}
+	template <typename type_1, typename... type_2>
+	inline constexpr auto greatest_common_divisor(type_1 first, type_2... second) noexcept {
+		return greatest_common_divisor(first, greatest_common_divisor(second...));
+	}
 }
 
 //_CONSTEXPR20 void swap(_Ty& _Left, _Ty& _Right) noexcept(
