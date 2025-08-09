636155c322e9a8d905752d5f6e2bc2f1e464bea7
oltex <80347929+oltex@users.noreply.github.com>
2025-08-09 19:40:05 +0900

    250809

--- data-structure/array/array.h
+++ data-structure/array/array.h
@@ -1,39 +1,49 @@
 #pragma once
 #include <memory>
+#include "../../memory/memory.h"
 
 namespace library {
 	template<typename type, size_t _size>
-	class array final {
-	public:
-		using iterator = type*;
-	private:
+	class array {
+	protected:
 		using size_type = unsigned int;
+		using iterator = type*;
+		type _array[_size];
 	public:
+		inline explicit array(void) noexcept = default;
+		inline explicit array(array const& rhs) noexcept = default;
+		inline explicit array(array&& rhs) noexcept = default;
+		inline auto operator=(array const& rhs) noexcept -> array & = default;
+		inline auto operator=(array&& rhs) noexcept -> array & = default;
+		inline ~array(void) noexcept = default;
+
 		inline auto operator[](size_type const index) noexcept ->type& {
-			return _arr[index];
+			return _array[index];
 		}
 		inline auto front(void) const noexcept ->type& {
-			return _arr[0];
+			return _array[0];
 		}
 		inline auto back(void) const noexcept ->type& {
-			return _arr[_size - 1];
+			return _array[_size - 1];
 		}
 		inline auto begin(void) const noexcept -> iterator {
-			return _arr;
+			return _array;
 		}
 		inline auto end(void) const noexcept -> iterator {
-			return _arr + _size;
+			return _array + _size;
 		}
 		inline auto size(void) const noexcept -> size_t {
 			return _size;
 		}
 		inline void fill(type const& value) noexcept {
-			for (auto& iter : _arr)
+			for (auto& iter : _array)
 				iter = value;
 		}
 		inline void clear(void) noexcept {
-			memset(_arr, 0, sizeof(type) * _size);
+			library::memory_set(_array, 0, sizeof(type) * _size);
 		}
-		type _arr[_size];
+		inline auto data(void) noexcept -> type* {
+			return _array;
+		}
 	};
 }
--- data-structure/array/main.cpp
+++ data-structure/array/main.cpp
@@ -6,15 +6,12 @@
 #include <array>
 #include <iostream>
 
-class A
-{
-private:
-	library::array<int, 10> a;
-};
 
 int main(void) noexcept {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	std::array<int, 100> std_arr{ 1,2 };
-	std::array<int, 1000> arr;
+	library::array<int, 1000> arr;
+
+	void* test = arr.data();
 	return 0;
 }
