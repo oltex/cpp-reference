ee480ac317f470cfd262b4fa472a503f493ae33e
oltex <80347929+oltex@users.noreply.github.com>
2025-08-21 20:02:49 +0900

    250821

--- library/algorithm/bubble_sort.h
+++ library/algorithm/bubble_sort.h
@@ -1,6 +1,26 @@
 #pragma once
 #include "../function.h"
 
-template<typename iterator, auto predicate = library::less<decltype(*std::declval<iterator>())>>
-inline void bouble_sort(iterator first, iterator last) {
+namespace library {
+	template<typename iterator, auto predicate = library::less<decltype(*std::declval<iterator>())>>
+	inline void bubble_sort(iterator first, iterator last) noexcept {
+		if (first == last)
+			return;
+		auto end = last;
+		while (end != first) {
+			bool swap = false;
+			auto new_end = first;
+
+			for (auto iter = first, next = library::next(first); next != end; ++iter, ++next) {
+				if (predicate(*next, *iter)) {
+					library::swap(*iter, *next);
+					swap = true;
+					new_end = next;
+				}
+			}
+			end = new_end;
+			if (!swap)
+				break;
+		}
+	}
 }
--- library/algorithm/main.cpp
+++ library/algorithm/main.cpp
@@ -3,11 +3,13 @@
 #include <crtdbg.h>
 
 #include "example/select_sort.h"
+#include "example/bubble_sort.h"
 
 int main(void) noexcept {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	example::select_sort();
+	//example::select_sort();
+	example::bubble_sort();
 
 	return 0;
 }
